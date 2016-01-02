ee008632f4f0cd0689363142555bf915aeeb9c4c
Tiago Cardoso <tiagocardosos@gmail.com>
2016-01-02 14:33:01 -0200

    Autenticação

--- gulpfile.js
+++ gulpfile.js
@@ -30,7 +30,10 @@ config.vendor_path_js =[
     config.bower_path + '/angular-messages/angular-messages.min.js',
     config.bower_path + '/angular-bootstrap/ui-bootstrap.min.js',
     config.bower_path + '/angular-strap/dist/angular-strap.min.js',
-    config.bower_path + '/angular-strap/dist/modules/navbar.min.js'
+    config.bower_path + '/angular-strap/dist/modules/navbar.min.js',
+    config.bower_path + '/angular-cookies/angular-cookies.min.js',
+    config.bower_path + '/query-string/query-string.js',
+    config.bower_path + '/angular-oauth2/dist/angular-oauth2.min.js',
 ];
 
 /* CONFIGURAÇÃO CSS */
@@ -41,6 +44,20 @@ config.vendor_path_css =[
     config.bower_path + '/bootstrap/dist/css/bootstrap-theme.min.css'
 ];
 
+/*** CONFIGURAÇÃO HTML */
+config.build_path_html = config.build_path + '/views';
+
+
+/** Tarefa copia HMTL **/
+gulp.task('copy-html', function(){
+    gulp.src([
+            config.assets_path + '/js/views/**/*.html'
+        ])
+        .pipe(gulp.dest(config.build_path_html))
+        .pipe(livereload());
+});
+
+/** Tarefa copia ESTILOS **/
 gulp.task('copy-styles', function(){
     gulp.src([
         config.assets_path + '/css/**/*.css'
@@ -54,6 +71,7 @@ gulp.task('copy-styles', function(){
         .pipe(livereload());
 });
 
+/** Tarefa copia JS **/
 gulp.task('copy-scripts', function(){
     gulp.src([
         config.assets_path + '/js/**/*.js'
@@ -73,6 +91,7 @@ gulp.task('clear-build-folder', function(){
 
 gulp.task('default', ['clear-build-folder'], function(){
 
+    gulp.start('copy-html');
     elixir(function(mix){
        mix.styles(config.vendor_path_css.concat([config.assets_path + '/css/**/*.css']),
             'public/css/all.css', config.assets_path);
@@ -87,8 +106,8 @@ gulp.task('default', ['clear-build-folder'], function(){
 
 gulp.task('watch-dev', ['clear-build-folder'], function(){
     livereload.listen();
-    gulp.start('copy-styles', 'copy-scripts');
-    gulp.watch(config.assets_path + '/**', ['copy-styles', 'copy-scripts']);
+    gulp.start('copy-styles', 'copy-scripts', 'copy-html');
+    gulp.watch(config.assets_path + '/**', ['copy-styles', 'copy-scripts', 'copy-html']);
 });
 
 /*
